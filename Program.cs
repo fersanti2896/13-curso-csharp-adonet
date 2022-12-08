ab414b3e2e23c373e58f2cb3cf74a3714a9f6db6
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-08 00:28:58 -0600

    Insertando data en la Base de Datos en Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ Console.WriteLine("¡ADO.NET Y BASES DE DATOS!");
 using IHost host = Host.CreateDefaultBuilder(args).Build();
 
 var configuration = host.Services.GetService<IConfiguration>();
-var conexion = configuration.GetConnectionString("cadenaConexion");
+var conexion = configuration!.GetConnectionString("cadenaConexion");
+
+Console.Write("Escribe el nombre a agregar: ");
+var nombre = Console.ReadLine();    
 
 try {
     using (SqlConnection connection = new SqlConnection(conexion)) { 
@@ -17,6 +20,17 @@ try {
         connection.Open();
 
         Console.WriteLine($"Conexión Abierta");
+
+        var query = @"INSERT INTO Personas(Nombre)
+                      VALUES(@Nombre)";
+
+        using (SqlCommand command = new SqlCommand(query, connection)) {
+            command.Parameters.Add(new SqlParameter("@Nombre", nombre));
+
+            var filas = await command.ExecuteNonQueryAsync();
+
+            Console.WriteLine($"Filas afectadas: { filas }");
+        }
     }
 } catch (Exception ex) {
     Console.WriteLine($"No se pudo abrir la conexión");
