238772c54ac4892d624b62ad329b2c4b9dc2756c
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-07 18:11:58 -0600

    Implementando conexión a SQL Server en appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 Console.WriteLine("¡ADO.NET Y BASES DE DATOS!");
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+var configuration = host.Services.GetService<IConfiguration>();
+var conexion = configuration.GetConnectionString("cadenaConexion");
+
+try {
+    using (SqlConnection connection = new SqlConnection(conexion)) { 
+        /* Abrimos la conexión */
+        connection.Open();
+
+        Console.WriteLine($"Conexión Abierta");
+    }
+} catch (Exception ex) {
+    Console.WriteLine($"No se pudo abrir la conexión");
+    Console.WriteLine($"Msg = { ex.Message }");
+}
